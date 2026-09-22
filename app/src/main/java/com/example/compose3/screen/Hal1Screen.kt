a7686f1cea332a3e25d8ee5287bf604ff72bf042
HeheXD1101 <mkharomen@gmail.com>
2026-09-22 11:29:23 +0700

    nambahin

--- app/src/main/java/com/example/compose3/screen/Hal1Screen.kt
+++ app/src/main/java/com/example/compose3/screen/Hal1Screen.kt
@@ -24,8 +24,8 @@ fun Hal1Screen(onNavigateToHal2: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "NIM: [NIM Placeholder]", style = MaterialTheme.typography.titleMedium)
-        Text(text = "Nama: [Nama Placeholder]", style = MaterialTheme.typography.titleMedium)
+        Text(text = "NIM: 245150407111037", style = MaterialTheme.typography.titleMedium)
+        Text(text = "Nama: Muhammad Murfid Kharomen", style = MaterialTheme.typography.titleMedium)
         Spacer(modifier = Modifier.size(20.dp))
         Text(text = "Screen 1")
         Spacer(modifier = Modifier.size(10.dp))
